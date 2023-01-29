9597cd40499b5dcd28af1c739a9025e265e5185a
Chaimaa HDIDOU <c.hdidou@b3gtech.com>
2023-01-29 23:51:57 +0100

    TES

--- src/main/java/com/example/tp4devops/Tp4devopsApplication.java
+++ src/main/java/com/example/tp4devops/Tp4devopsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Tp4devopsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Tp4devopsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Tp4devopsApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/tp4devops/Tp4devopsApplicationTests.java
+++ src/test/java/com/example/tp4devops/Tp4devopsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Tp4devopsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
