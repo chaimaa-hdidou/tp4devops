073a1474a73250202e13939ffde99c88bc5a2ab2
Chaimaa HDIDOU <c.hdidou@b3gtech.com>
2023-01-29 23:00:13 +0100

    Initial commit

---
+++ src/main/java/com/example/tp4devops/Tp4devopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp4devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4devopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4devopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tp4devops/Tp4devopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp4devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4devopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
